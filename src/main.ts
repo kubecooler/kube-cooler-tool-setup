4ab6cdf2b3870d7fad01dc1162cc050c98d98d0a
Mikedu1988 <duliyang@juzhou.io>
2021-07-08 17:32:28 +0800

    ci test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {exec} from '@actions/exec'
+import * as fs from 'fs'
+import * as yaml from 'yaml'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`Waiting ${ms} milliseconds mike !!!...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const fileContents = fs.readFileSync('./tools.yaml', 'utf-8')
+    const data = yaml.parse(fileContents)
+    for (const tool of data.tools) {
+      //if there is a command, use the command directly
+      if (tool.command) {
+        await exec(tool.command)
+      }
+    }
+    // core.info(yaml.stringify(data.tools[0].name))
   } catch (error) {
     core.setFailed(error.message)
   }

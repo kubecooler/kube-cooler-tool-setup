62620a938bd722cc0ffb2466a94216b1ac9d2832
Mikedu1988 <duliyang@juzhou.io>
2021-07-08 18:00:23 +0800

    ci test

--- dist/index.js
+++ dist/index.js
@@ -49,11 +49,11 @@ function run() {
         try {
             const ms = core.getInput('milliseconds');
             core.debug(`Waiting ${ms} milliseconds mike !!!...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-            const fileContents = fs.readFileSync('./tools.yaml', 'utf-8');
+            const fileContents = fs.readFileSync(path_1.default.join(__dirname, '..', 'tools', 'tools.yaml'), 'utf-8');
             const data = yaml.parse(fileContents);
             for (const tool of data.tools) {
                 //if there is a command, use the command directly
-                yield exec_1.exec(path_1.default.join(__dirname, 'tools', tool.name, 'install.sh'));
+                yield exec_1.exec(path_1.default.join(__dirname, '..', 'tools', tool.name, 'install.sh'));
             }
             // core.info(yaml.stringify(data.tools[0].name))
         }
